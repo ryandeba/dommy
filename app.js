8eb5df2f3056e6ca18c15635d4c011e397b51bb2
Ryan Deba <ryandeba@gmail.com>
2018-01-11 22:17:30 -0600

    dance party 2.0

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
+const animations = [
+  "bounce", "flash", "pulse", "rubberBand", "shake",
+  "headShake", "swing", "tada", "wobble", "jello",
+  "bounceIn", "bounceInDown", "bounceInLeft", "bounceInRight", "bounceInUp",
+  "bounceOut", "bounceOutDown", "bounceOutLeft", "bounceOutRight", "bounceOutUp",
+  "fadeIn", "fadeInDown", "fadeInDownBig", "fadeInLeft", "fadeInLeftBig",
+  "fadeInRight", "fadeInRightBig", "fadeInUp", "fadeInUpBig", "fadeOut",
+  "fadeOutDown", "fadeOutDownBig", "fadeOutLeft", "fadeOutLeftBig", "fadeOutRight",
+  "fadeOutRightBig", "fadeOutUp", "fadeOutUpBig", "flipInX", "flipInY",
+  "flipOutX", "flipOutY", "lightSpeedIn", "lightSpeedOut", "rotateIn",
+  "rotateInDownLeft", "rotateInDownRight", "rotateInUpLeft", "rotateInUpRight", "rotateOut",
+  "rotateOutDownLeft", "rotateOutDownRight", "rotateOutUpLeft", "rotateOutUpRight", "hinge",
+  "rollIn", "rollOut", "zoomIn", "zoomInDown",
+  "zoomInLeft", "zoomInRight", "zoomInUp", "zoomOut", "zoomOutDown",
+  "zoomOutLeft", "zoomOutRight", "zoomOutUp", "slideInDown", "slideInLeft",
+  "slideInRight", "slideInUp", "slideOutDown", "slideOutLeft", "slideOutRight",
+  "slideOutUp"
+];
+
+Vue.component("dommy", {
+  template: "#template-dommy",
+  props: ["data"],
+  computed: {
+    dommyClasses: function() {
+      var classes = ["animated", this.data.animation];
+      this.data.repeatAnimation && classes.push("animated--repeat");
+      return classes.join(" ");
+    },
+  }
+});
+
 new Vue({
   el: "#app",
   data: {
     animation: "",
-    animations: [
-      "bounce", "flash", "pulse", "rubberBand", "shake",
-      "headShake", "swing", "tada", "wobble", "jello",
-      "bounceIn", "bounceInDown", "bounceInLeft", "bounceInRight", "bounceInUp",
-      "bounceOut", "bounceOutDown", "bounceOutLeft", "bounceOutRight", "bounceOutUp",
-      "fadeIn", "fadeInDown", "fadeInDownBig", "fadeInLeft", "fadeInLeftBig",
-      "fadeInRight", "fadeInRightBig", "fadeInUp", "fadeInUpBig", "fadeOut",
-      "fadeOutDown", "fadeOutDownBig", "fadeOutLeft", "fadeOutLeftBig", "fadeOutRight",
-      "fadeOutRightBig", "fadeOutUp", "fadeOutUpBig", "flipInX", "flipInY",
-      "flipOutX", "flipOutY", "lightSpeedIn", "lightSpeedOut", "rotateIn",
-      "rotateInDownLeft", "rotateInDownRight", "rotateInUpLeft", "rotateInUpRight", "rotateOut",
-      "rotateOutDownLeft", "rotateOutDownRight", "rotateOutUpLeft", "rotateOutUpRight", "hinge",
-      "rollIn", "rollOut", "zoomIn", "zoomInDown",
-      "zoomInLeft", "zoomInRight", "zoomInUp", "zoomOut", "zoomOutDown",
-      "zoomOutLeft", "zoomOutRight", "zoomOutUp", "slideInDown", "slideInLeft",
-      "slideInRight", "slideInUp", "slideOutDown", "slideOutLeft", "slideOutRight",
-      "slideOutUp"
-    ],
-    danceAnimations: [
-      "bounce", "pulse", "rubberBand", "shake",
-      "swing", "tada", "wobble", "jello", "lightSpeedIn"
-    ],
+    animations: animations,
+    numberOfDommys: 1,
+    dommys: [new Dommy()],
     repeatAnimation: false,
     danceParty: false,
     dancePartyInterval: undefined,
@@ -43,31 +55,45 @@ new Vue({
     ],
     appStyle: ""
   },
-  computed: {
-    dommyClasses: function() {
-      var classes = ["animated", this.animation];
-      (this.repeatAnimation || this.danceParty) && classes.push("animated--repeat");
-      return classes.join(" ");
-    },
-    availableDanceAnimations: function() {
-      var index = this.danceAnimations.indexOf(this.animation);
-      return index && this.danceAnimations.splice(index, 1) || this.danceAnimations;
-    }
-  },
   watch: {
     danceParty: function() {
       if (this.danceParty) {
+        this.repeatAnimation = true;
         this.shuffle();
         this.dancePartyInterval = setInterval(this.shuffle, 2000);
       } else {
         clearInterval(this.dancePartyInterval);
         this.appStyle = "background-color: initial";
       };
+    },
+    animation: function() {
+      var animation = this.animation;
+      this.dommys.forEach(function(dommy) {
+        console.log(animation);
+        dommy.animation = animation;
+      });
+    },
+    repeatAnimation: function() {
+      var repeatAnimation = this.repeatAnimation;
+      this.dommys.forEach(function(dommy) {
+        dommy.repeatAnimation = repeatAnimation;
+      });
+    },
+    numberOfDommys: function() {
+      while (this.dommys.length < this.numberOfDommys) {
+        this.dommys.push(new Dommy({animation: this.animation, repeatAnimation: this.repeatAnimation}));
+      };
+
+      while (this.dommys.length > this.numberOfDommys && this.dommys.length > 1) {
+        this.dommys.pop();
+      };
     }
   },
   methods: {
     shuffle: function() {
-      this.animation = getRandomArrayElement(this.danceAnimations);
+      this.dommys.forEach(function(dommy) {
+        dommy.animation = getRandomArrayElement(animations);
+      });
       this.appStyle = "transition: 2s all; background-color: " + getRandomArrayElement(this.dancePartyColors);
     }
   }
@@ -77,7 +103,16 @@ function getRandomArrayElement(array) {
   return array[Math.floor(Math.random() * array.length)];
 };
 
+function Dommy(options) {
+  return {
+    animation: "",
+    repeatAnimation: false,
+    ...options
+  };
+};
+
 /*
 ideas
 - options for clothing/accessories (hats, food, etc)
+- sad-faced disabled dommy when there are no dommys
 */
